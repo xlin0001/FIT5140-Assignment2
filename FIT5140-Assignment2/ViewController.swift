21cacc150128ddc1aa0089ab6126d965f8f59a45
xlin0001 <xlin0003@student.monash.edu>
2018-09-15 16:40:17 +1000

    Initial Commit

---
+++ FIT5140-Assignment2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FIT5140-Assignment2
+//
+//  Created by Oliver Lin on 15/9/18.
+//  Copyright © 2018 Monash University. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
