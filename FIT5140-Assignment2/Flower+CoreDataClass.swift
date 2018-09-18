dc4bdc17328e6f837a3ff2fe1c0c94b76098a6e6
yufan <syf941020@gmail.com>
2018-09-18 22:00:00 +1000

    flower class and database

---
+++ FIT5140-Assignment2/Flower+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Flower+CoreDataClass.swift
+//  
+//
+//  Created by 沈宇帆 on 2018/9/18.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Flower)
+public class Flower: NSManagedObject {
+
+}
